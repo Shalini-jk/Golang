805e3caf3ba6561a50699637e67e25e31c237084
Shalini-jk <127472079+Shalini-jk@users.noreply.github.com>
2023-12-27 17:45:28 +0530

    Update random.go

--- BasicTopics/6_packages/Problems/Random_Number/random/random.go
+++ BasicTopics/6_packages/Problems/Random_Number/random/random.go
@@ -10,7 +10,7 @@ func Random_number_generator() {
 	// Seed the random number generator with the current time
 	rand.Seed(time.Now().UnixNano())
 
-	// Generate a random number between 1 and 100
+	// Generate a random number between 1 and 9999
 	randomNumber := rand.Intn(9999) + 1
 	fmt.Println("The random number", randomNumber)
 }
