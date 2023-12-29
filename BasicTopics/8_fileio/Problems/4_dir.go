08a3b29ef80b4296b21ec45e1d6dc016057c10ac
shalini <shalini@ambersoft.in>
2023-12-29 09:24:02 +0530

    new changes in package folder

--- BasicTopics/8_fileio/Problems/4_dir.go
+++ BasicTopics/8_fileio/Problems/4_dir.go
@@ -2,21 +2,3 @@
 Write a program that generates a tree-like structure representing the hierarchy of files and directories starting from a specified root directory. Use os and os.FileInfo to gather information about files and directories.
 */
 
-Package main
-
-import (
-    "fmt"
-    "os"
-	"io"
-)
-
-func main() {
-    files, err := os.ReadDir("8_fileio")
-    if err != nil {
-        fmt.Println("Error reading directory:", err)
-        return
-    }
-    for _, file := range files {
-        fmt.Println(file.Name())
-    }
-}
